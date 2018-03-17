eca37a837c7c80d52b2a797aee4b91c91afb77a4
aayush1408 <aayushjaiswal0114@gmail.com>
2018-03-17 17:47:45 +0530

    database setting made

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const user = require('./routes/user.js');
+const config = require('./config/database.js');
+
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected ' + config.database);
+});
+
+mongoose.connection.on('error', () => {
+    console.log('Error connecting to db ' + err);
+});
 
 const app = express();
 
@@ -18,6 +29,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Bodyparser 
 app.use(bodyParser.json());
+
+//setup home route 
+app.use('/', (req, res) => {
+    res.send('Home');
+});
 
 const port = process.env.PORT || 3000;
 
